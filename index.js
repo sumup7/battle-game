7b62b5c8690162cfd69e0c7913aced93a211b3f4
Tomohiro Sasaya <sasaya7@gmail.com>
2020-08-30 10:24:43 +0900

    ほぼ完成形

--- index.js
+++ index.js
@@ -1,85 +1,415 @@
 'use strict';
+let turn = 0;
+let msW;
+let enemyDamage = 0;
+let playerDamage = 0;
+let op1;
+let op2;
+let op3;
+let s;
+let m;
+let re;
 
-let option1 = "風を使う";
-let option2 = "火を使う";
-let option3 = "水を使う";
+// const points = [
+//   100,100,
+//   300,100,
+//   100,300,
+//   300,300
+// ]
 
-
-let canvas = document.body.querySelector('#draw');
+const canvas = document.body.querySelector('#draw');
 let ctx = canvas.getContext('2d');
 ctx.fillRect(50, 50, 800, 800);
 
-let msWindow = document.body.querySelector('#messageWindow');
-let msw = msWindow.getContext('2d');
+enemy();
+player();
 
-function messageWindow(e) {
+function player() {
+ctx.fillStyle = 'red';
+ctx.beginPath(350, 350);
+// パスの始点を設定する
+ctx.moveTo(350, 350);
+// 各頂点を結ぶパスを設定する
+// for(let i = 2; i < points.length; i += 2){
+//     ctx.lineTo(points[i], points[i + 1]);
+//     console.log(points[i],points[i + 1]);
+// }
+// ctx.lineTo(300, 100);
+ctx.lineTo(400, 250);
+ctx.lineTo(450, 350);
+
+// パスを閉じることを明示する
+ctx.closePath(350, 350);
+// 設定したパスで多角形の描画を行う
+ctx.fill();
+}
+
+function enemy() {
+ctx.fillStyle = 'yellow';
+ctx.beginPath(100, 100);
+// パスの始点を設定する
+ctx.moveTo(100, 100);
+// 各頂点を結ぶパスを設定する
+// for(let i = 2; i < points.length; i += 2){
+//     ctx.lineTo(points[i], points[i + 1]);
+//     console.log(points[i],points[i + 1]);
+// }
+// ctx.lineTo(300, 100);
+ctx.lineTo(50, 100);
+ctx.lineTo(150, 100);
+// ctx.lineTo(100, 300);
+ctx.lineTo(150, 250);
+ctx.lineTo(250, 250);
+ctx.lineTo(100, 100);
+// パスを閉じることを明示する
+ctx.closePath(100, 100);
+// 設定したパスで多角形の描画を行う
+ctx.fill();
+}
+
+const msWindow = document.body.querySelector('#messageWindow');
+const msw = msWindow.getContext('2d');
+
+// メッセージウィンドウの描画
+function  messageWindow(){
   msw.fillStyle = 'black';							//	ウィンドウの色
   msw.fillRect(50, 50, 800, 800);					//	矩形描画
-						
+}
+messageWindow();
+//自分のキャラのステータスのオブジェクト
+const playerStatus = {
+  hp: 100,
+  offensive: 100,
+  defense: 50,
+  speed: 90
+};
+//敵キャラのステータスのオブジェクト
+const enemyStatus = {
+  hp: 100,
+  offensive: 90,
+  defense: 70,
+  speed: 100
+};
+
+let currentPlayerHp = playerStatus.hp;
+let currentEnemyHp = enemyStatus.hp;
+let currentPlayerSpeed = playerStatus.speed;
+let currentEnemySpeed = enemyStatus.speed;
+
+turnGame();
+
+function gameSet() {
+   //どちらかか両方のhpが０以下になった時引き分けまたは勝敗を表示してゲームが終わる処理
+   if (currentPlayerHp <= 0 && currentEnemyHp <= 0) {
+    // 双方相討ちで引き分けになったというメッセージを出す
+    ;
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('お互いのHPがゼロになっった。引き分けになった。')
+    restartButton();
+  } else if (currentEnemyHp <= 0) {
+    // 敵のライフがゼロ以下になった。勝った。というメッセージを出す
+    ;
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('敵のライフがゼロ以下になった。勝った。');
+    restartButton();
+  } else if (currentPlayerHp <= 0) {
+    // 自分のライフがゼロ以下になった。負けた。というメッセージを出す
+    ;
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('自分のライフがゼロ以下になった。負けた。');
+    restartButton();
+    }else if(turn === 1 && currentPlayerSpeed > enemyStatus.speed){
+      turn = 2;
+      turnGame();
+    }else if(turn === 1 && currentPlayerSpeed < enemyStatus.speed){
+      turn = 3;
+      turnGame();
+    }else if(turn === 2 || turn === 3 || turn === 5){
+      turn = 0;
+      turnGame();
+    }else if(turn === 4 && currentPlayerSpeed > enemyStatus.speed){
+      turn = 2;
+      turnGame();
+    // } else if (turn === 3) {
+    //   turn = 0;
+    //   turnGame();
+    } else if(turn === 4 && currentPlayerSpeed < enemyStatus.speed) {
+      turn = 5;
+      turnGame();
+    // } else if(true === 5) {
+    //   turn = 0;
+    //   turnGame();
+    }
+    else {
+  ;
+  statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+  optionButton1();
+  optionButton2();
+  optionButton3();}
 }
 
-function optionButton1(x, y, width, height) {
-  let oButton = document.body.querySelector('#option1');
-  let ob1 = oButton.getContext('2d');
-  ob1.font = "50px 'ＭＳ ゴシック'"; 									//	文字フォントを設定
-  ob1.fillStyle = 'red'; //	文字色
-  //ob1.rect(60, 60, 100, 100);
-  //ob1.stroke();
-  ob1.fillText(option1, 60, 120);
-  oButton.addEventListener('click', function (e) {
-    // let button = e.target.getBoundingClientRect();
-    //  mouseX = e.clientX - button.left;
-    //  mouseY = e.clientY - button.top;
-    // if (x < mouseX && mouseX < x + width) {
-    //   if (y < mouseY && mouseY < y + height) {
-        let one = 1;
-        console.log(one);
-})}
+// ターン制の戦闘処理
+function turnGame() {
+ 
+  if (turn === 0
+    ) {
+    gameSet();
+  } else if (turn === 1 && currentPlayerSpeed > enemyStatus.speed) {
+    //技１を選んで自分のスピードが敵のスピードを上まって先攻の場合
+    ;
+    waza1damage();
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('先手をとった。重くて遅い一撃' + enemyDamage + 'のダメージ');
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();
+    }
+  } else if (turn === 1 && currentPlayerSpeed < enemyStatus.speed) {
+    //技１を選んで相手のスピードが自分のスピードを上まった後攻の場合
+    ;
+    enemyOffensive();
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('素早さで負けた。相手の攻撃' + playerDamage + 'のダメージ');
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();
+    }
+  } else if (turn === 2) {
+    //先攻した時の相手の攻撃
+    ;
+    enemyOffensive();
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('相手の攻撃のターン。相手の攻撃' + playerDamage + 'のダメージ');
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();
+    }
+  } else if (turn === 3) {
+    //相手に先攻された時の後攻の攻撃
+    ;
+    waza1damage();
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    message('自分の攻撃のターン。重くて遅い一撃' + enemyDamage + 'のダメージ');
+    // statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();
+    }
+  } else if (turn === 4 && currentPlayerSpeed > enemyStatus.speed) {
+    //技２で相手のスピードを上回り先攻した場合
+    ;
+    waza2damage();
+    message('先手をとった。素早くて軽い攻撃' + enemyDamage + 'のダメージ。自分のスピードが20上がった。');
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();
+    }
+  } else if (turn === 4 && currentPlayerSpeed < enemyStatus.speed) {
+    //技２でスピード負けて相手の先攻になった場合
+    ;
+    enemyOffensive();
+    message('素早さで負けた。相手の攻撃' + playerDamage + 'のダメージ');
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();
+    }
+  } else if(turn === 5) {
+    //技２で後攻になった時の自分の攻撃のターン
+    ;
+    waza2damage();
+    message('自分の攻撃のターン。素早くて軽い攻撃' + enemyDamage + 'のダメージ。自分のスピードが20上がった。');
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    document.body.onkeydown = function () {
+      msW.removeChild(s);
+      msW.removeChild(m);
+      gameSet();}
+    }else if(turn === 6 && currentPlayerSpeed > enemyStatus.speed){
+    //技３で相手のスピードを上まって先に回復を使う場合
+    ;
+    waza3recoveryHp();
+    message('先手をとった。自分のHPを回復。HPが50回復して'+ currentPlayerHp + 'になっった。');
+    statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+    document.body.onkeydown = function () {
+      turn = 2;
+      msW.removeChild(s);
+      msW.removeChild(m);
+      turnGame();}
+    }else if(turn === 6 && currentPlayerSpeed < enemyStatus.speed) {
+      //技３で相手のスピードが上まって先攻される場合
+      ;
+      enemyOffensive();
+      message('先手を取られた。相手の攻撃' + playerDamage + 'のダメージ');
+      statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+      document.body.onkeydown = function () {
+        turn = 7;
+        msW.removeChild(s);
+        msW.removeChild(m);
+        turnGame();}
+    }else if(turn === 7) {
+      //技３で先攻された場合の自分の回復ターン
+      ;
+      waza3recoveryHp();
+      message('自分のターン。自分のHPを回復。HPが50回復した'+ currentPlayerHp + 'になっった。');
+      statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed);
+      document.body.onkeydown = function () {
+        turn = 0;
+        msW.removeChild(s);
+        msW.removeChild(m);
+        turnGame();}
+    } 
+    }
 
-function optionButton2(x, y, width, height) {
-  let oButton2 = document.body.querySelector('#option2');
-  let ob2 = oButton2.getContext('2d');
-  ob2.font = "50px 'ＭＳ ゴシック'"; 									//	文字フォントを設定
-  ob2.fillStyle = 'red'; //	文字色
-  //ob2.rect(60, 160, 100, 100);
-  //ob1.stroke();
-  ob2.fillText(option2, 60, 220);
-  oButton2.addEventListener('click', function (e) {
-//     // let button = e.target.getBoundingClientRect();
-//     //  mouseX = e.clientX - button.left;
-//     //  mouseY = e.clientY - button.top;
-//     // if (x < mouseX && mouseX < x + width) {
-//     //   if (y < mouseY && mouseY < y + height) {
-        let one = 2;
-        console.log(one);
-})}
+    //技１の選択肢を選んだ時の処理
+    function waza1damage() {
+      enemyDamage = (playerStatus.offensive - enemyStatus.defense) * 1.5;
+      console.log(enemyDamage);
+      currentEnemyHp = currentEnemyHp - enemyDamage;
+    }
+    //技２の選択肢を選んだ時の処理
+    function waza2damage() {
+      enemyDamage = playerStatus.offensive - enemyStatus.defense;
+      currentPlayerSpeed = currentPlayerSpeed + 20;
+      currentEnemyHp = currentEnemyHp - enemyDamage;
+    }
+    //技３の選択肢を選んだ時の処理
+    function waza3recoveryHp() {
+      currentPlayerHp = currentPlayerHp + 50;
+    }
+    //敵の攻撃
+    function enemyOffensive() {
+      playerDamage = enemyStatus.offensive - playerStatus.defense;
+      currentPlayerHp = currentPlayerHp - playerDamage;
+    }
+    //自分と敵のHPとスピードを表示する処理
+    function statusDraw(currentPlayerHp, currentEnemyHp,currentPlayerSpeed,currentEnemySpeed) {
+      msW = msWindow.parentNode;
+      s = document.createElement('div');
+      s.setAttribute('id', 'status');
+      s.style.position = 'absolute';
+      s.style.top = '680px';
+      s.style.left = '60px';
+      s.style.width = '800px';
+      s.style.height = '100px';
+      s.style.color = 'white';
+      s.style.font = '28px monospace';
+      msW.appendChild(s);
+      s.innerHTML = '自分のHP' + currentPlayerHp + 'スピード'+ currentPlayerSpeed + ' ' + '敵のHP' + currentEnemyHp + 'スピード' + currentEnemySpeed;
+    }
 
-function optionButton3(x, y, width, height) {
-  let oButton3 = document.body.querySelector('#option3');
-  let ob3 = oButton3.getContext('2d');
-  ob3.font = "50px 'ＭＳ ゴシック'"; 									//	文字フォントを設定
-  ob3.fillStyle = 'red'; //	文字色
-  //ob3.rect(60, 260, 100, 100);
-  //ob1.stroke();
-  ob3.fillText(option3, 60, 320);
-  // const a = document.createElement('h3');
-  // a.id = oButton3;
-  // let option1 = "風を使う";
-  // oButton3 = option1;
-  oButton3.addEventListener('click', function (e) {
-    let button = e.target.getBoundingClientRect();
-    //  mouseX = e.clientX - button.left;
-    //  mouseY = e.clientY - button.top;
-    // if (x < mouseX && mouseX < x + width) {
-    //   if (y < mouseY && mouseY < y + height) {
-        let one = 3;
-        console.log(one);
-})}
+    //メッセージを描画する関数
+    function message(textMessage) {
+      msW = msWindow.parentNode;
+      m = document.createElement('div');
+      m.setAttribute('id', 'message');
+      m.style.position = 'absolute';
+      m.style.top = '750px';
+      m.style.left = '60px';
+      m.style.width = '700px';
+      m.style.height = '100px';
+      m.style.background = 'transparent';
+      m.style.color = 'white';
+      m.style.textAlign = 'center';
+      m.style.font = '50px monospace';
+      msW.appendChild(m);
+      m.innerHTML = textMessage;
+      console.log(textMessage);
+    }
+    //画面の初期情報を消す関数
+    function eraser() {
+      msW.removeChild(op1);
+      msW.removeChild(op2);
+      msW.removeChild(op3);
+      msW.removeChild(s);
+    }
+    //技１を選択するボタン
+    function optionButton1() {
+      msW = msWindow.parentNode;
+      op1 = document.createElement('div');
+      op1.setAttribute('id', 'option1');
+      op1.style.position = 'absolute';
+      op1.style.top = '730px';
+      op1.style.left = '60px';
+      op1.style.width = '400px';
+      op1.style.height = '300px';
+      // op1.style.background = 'transparent';
+      op1.style.color = 'white';
+      op1.style.textAlign = 'left';
+      op1.style.font = '50px monospace';
+      msW.appendChild(op1);
+      op1.innerHTML = "重く遅い一撃";
+      op1.addEventListener('click', function () {
+        eraser();
+        turn = 1;
+        turnGame();
+        // console.log(turn);
 
-window.onload = function () {
-        messageWindow();
-       optionButton1();
-       optionButton2();
-       optionButton3();
-         // Window上のマウス座標を取得する
-}
+      })}
+
+      //技２を選択するボタン２
+      function optionButton2() {
+        msW = msWindow.parentNode;
+        op2 = document.createElement('div');
+        op2.setAttribute('id', 'option2');
+        op2.style.position = 'absolute';
+        op2.style.top = '820px';
+        op2.style.left = '60px';
+        op2.style.width = '500px';
+        op2.style.height = '300px';
+        op2.style.color = 'white';
+        op2.style.textAlign = 'left';
+        op2.style.font = '50px monospace';
+        msW.appendChild(op2);
+        op2.innerHTML = "素早く軽い攻撃";
+        op2.addEventListener('click', function () {
+          eraser();
+          turn = 4;
+          turnGame();
+        })
+      }
+      //技３を選択するボタン３
+      function optionButton3() {
+        msW = msWindow.parentNode;
+        op3 = document.createElement('div');
+        op3.setAttribute('id', 'option3');
+        op3.style.position = 'absolute';
+        op3.style.top = '930px';
+        op3.style.left = '60px';
+        op3.style.width = '400px';
+        op3.style.height = '300px';
+        op3.style.background = 'transparent';
+        op3.style.color = 'white';
+        op3.style.textAlign = 'left';
+        op3.style.font = '50px monospace';
+        msW.appendChild(op3);
+        op3.innerHTML = "回復";
+        op3.addEventListener('click', function () {
+          eraser();
+          turn = 6;
+　　　　　　turnGame();
+        })}
+    //技１を選択するボタン
+    function restartButton() {
+      msW = msWindow.parentNode;
+      re = document.createElement('div');
+      re.setAttribute('id', 'restart');
+      re.style.position = 'absolute';
+      re.style.top = '910px';
+      re.style.left = '60px';
+      re.style.width = '550px';
+      re.style.height = '300px';
+      re.style.color = 'pink';
+      re.style.textAlign = 'left';
+      re.style.font = '40px monospace';
+      msW.appendChild(re);
+      re.innerHTML = "もう一度戦うならここをクリック";
+      re.addEventListener('click', function () {
+        location.reload();
+      })}        
